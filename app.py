8da7ab698ef8191807229e7f782615cd975f7a26
Martin Hernandez <mhernandez@intersysconsulting.com>
2019-09-02 13:12:41 -0500

    Endpoints users

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
-#Add the blueprints, i., products from views
-from views.products import products
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager
+import datetime
 
-#Creates the app and register the blueprint
-app = Flask(__name__)
-app.register_blueprint(products)
+# Add the blueprints, i.e, products from views
+# from views.products import products
+# from views.users import users
 
-@app.route("/")
-def home():
-    return "<title>Hello ecommerce</title> <center><h1>Welcome</h1> <br> ecommerce Api Endpoint V1<center>"
+# from common.db import mongo
+
+
+def create_app(config_filename):
+    # Creates the app.
+    app = Flask(__name__)
+    app.config['MONGO_URI'] = 'mongodb://localhost:27017/ecommerce'
+    app.config['JWT_SECRET_KEY'] = 'secret_pass'
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+    jwt = JWTManager(app)
+    #app.config['MONGO_URI'] = 'mongodb+srv://aintersys:aintersys@e-commerce-lcqki.mongodb.net/test?retryWrites=true&w=majority'
+    #app.config['MONGO_URI'] = 'mongodb://aintersys:aintersys@e-commerce-shard-00-00-lcqki.mongodb.net:27017,e-commerce-shard-00-01-lcqki.mongodb.net:27017,e-commerce-shard-00-02-lcqki.mongodb.net:27017/test?ssl=true&replicaSet=e-commerce-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+    from common.db import mongo
+    mongo.init_app(app)
+
+    from views.products import products
+    from views.users import users
+    app.register_blueprint(products)
+    app.register_blueprint(users)
+
+    @app.route('/')
+    def home():
+        return '<title>Hello ecommerce</title> <center><h1>Welcome</h1> <br> ecommerce Api Endpoint V1<center>', 200
+
+    return app
+
+# app.config['JWT_SECRET_KEY'] = 'secret_pass'
+# app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+# mongo = PyMongo(app)
+# jwt = JWTManager(app)
--- views/products.py
+++ views/products.py
@@ -1,10 +1,24 @@
 # Import flask and necesary dependencies.
 from flask import Flask, Blueprint, request, jsonify
+from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity)
+from common.db import mongo
+from common.utils import *
 
 # Create the blueprint
-products = Blueprint("products", __name__)
+products = Blueprint('products', __name__)
 
 # Create the route
-@products.route("/products", methods=["GET", "POST"])
-def productB():
-    return jsonify({"Hello": "Products"})
+@products.route('/products', methods=['GET'])
+def productsAdmin():
+    if request.method == 'GET':
+        output = defaultObject()
+        # output.append({'name': 'Martin'})
+        for single_product in mongo.db.products.find():
+            single_product['_id'] = str(single_product['_id'])
+            single_product['createdAt'] = convertTimestampToDateTime(single_product['createdAt'])
+            single_product['updatedAt'] = convertTimestampToDateTime(single_product['updatedAt'])
+            output['data'].append(single_product)
+        return jsonify(output), 200
