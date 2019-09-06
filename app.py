7f2e267ba4210cdc242f3d848556fdb6894f4848
Martin Hernandez <mhernandez@intersysconsulting.com>
2019-09-06 14:06:02 -0500

    Aplied CORS, use of 'waitress' webserver.

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager
 import datetime
 
-# Add the blueprints, i.e, products from views
-# from views.products import products
-# from views.users import users
 
-# from common.db import mongo
-
-
-def create_app(config_filename):
+def create_app():
     # Creates the app.
     app = Flask(__name__)
+    CORS(app)
     #app.config['MONGO_URI'] = 'mongodb://localhost:27017/ecommerce'
     app.config['JWT_SECRET_KEY'] = 'secret_pass'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
@@ -36,8 +32,3 @@ def create_app(config_filename):
         return '<title>Hello ecommerce</title> <center><h1>Welcome</h1> <br> ecommerce Api Endpoint V1<center>', 200
 
     return app
-
-# app.config['JWT_SECRET_KEY'] = 'secret_pass'
-# app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
-# mongo = PyMongo(app)
-# jwt = JWTManager(app)
