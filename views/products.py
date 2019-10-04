bd345c677b9e31ec6a4a532e21ba31d913b93f8f
jandreshj <jandreshdzj@gmail.com>
2019-10-04 13:23:11 -0500

    Creates an endpoint that list the searched products

--- views/products.py
+++ views/products.py
@@ -3,7 +3,8 @@ from flask import Flask, Blueprint, request, jsonify
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity)
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                jwt_required, jwt_refresh_token_required, get_jwt_identity)
 
 from common.db import mongo
 from common.utils import *
@@ -34,7 +35,8 @@ def get_products():
 def get_products_paginated(total_items, page):
     if request.method == 'GET':
         output = defaultObjectDataAsAnObject()
-        output['data']['total_products'] = mongo.db.products.count_documents({'available': True})
+        output['data']['total_products'] = mongo.db.products.count_documents({
+                                                                             'available': True})
 
         products_array = []
         skip = (total_items * page) - total_items
@@ -60,13 +62,16 @@ def get_single_product():
         if (data['ok']):
             data = data['data']
             print(data)
-            searched_product = mongo.db.products.find_one({'_id': ObjectId(data['_id'])})
+            searched_product = mongo.db.products.find_one(
+                {'_id': ObjectId(data['_id'])})
             print(searched_product)
             if (searched_product):
                 del searched_product['_id']
                 del searched_product['sold']
-                searched_product['createdAt'] = convertTimestampToDateTime(searched_product['createdAt'])
-                searched_product['updatedAt'] = convertTimestampToDateTime(searched_product['updatedAt'])
+                searched_product['createdAt'] = convertTimestampToDateTime(
+                    searched_product['createdAt'])
+                searched_product['updatedAt'] = convertTimestampToDateTime(
+                    searched_product['updatedAt'])
                 output['message'] = 'CORRECT'
                 output['status'] = True
                 output['data'] = searched_product
@@ -85,12 +90,15 @@ def get_products_all():
     if request.method == 'GET':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             for single_product in mongo.db.products.find():
                 single_product['_id'] = str(single_product['_id'])
-                single_product['createdAt'] = convertTimestampToDateTime(single_product['createdAt'])
-                single_product['updatedAt'] = convertTimestampToDateTime(single_product['updatedAt'])
+                single_product['createdAt'] = convertTimestampToDateTime(
+                    single_product['createdAt'])
+                single_product['updatedAt'] = convertTimestampToDateTime(
+                    single_product['updatedAt'])
                 output['data'].append(single_product)
             return jsonify(output), 200
 
@@ -101,7 +109,8 @@ def create_product():
     if request.method == 'POST':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             data = request.get_json()
             data = validate_products_post(data)
@@ -131,7 +140,8 @@ def update_product():
     if request.method == 'PUT':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             data = request.get_json()
             data = validate_products_put(data)
@@ -163,14 +173,16 @@ def delete_product():
     if request.method == 'DELETE':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             data = request.get_json()
             data = validate_just_id(data)
             if data['ok']:
                 data = data['data']
                 data['updatedAt'] = datetime.timestamp(datetime.now())
-                update_product_availability = mongo.db.products.update_one({'_id': ObjectId(data['_id'])}, {'$set': {'available': False, 'updatedAt': data['updatedAt']}})
+                update_product_availability = mongo.db.products.update_one({'_id': ObjectId(
+                    data['_id'])}, {'$set': {'available': False, 'updatedAt': data['updatedAt']}})
                 if (update_product_availability.modified_count):
                     output['status'] = True
                     output['message'] = 'DELETED_CORRECTLY'
@@ -193,13 +205,15 @@ def delete_product_real():
     if request.method == 'DELETE':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             data = request.get_json()
             data = validate_just_id(data)
             if data['ok']:
                 data = data['data']
-                deleted_products = mongo.db.products.delete_one({'_id': ObjectId(data['_id'])})
+                deleted_products = mongo.db.products.delete_one(
+                    {'_id': ObjectId(data['_id'])})
                 if (deleted_products.deleted_count):
                     output['status'] = True
                     output['message'] = 'DELETED_CORRECTLY'
@@ -222,7 +236,8 @@ def upload_image():
     if request.method == 'POST':
         output = defaultObject()
         current_user = get_jwt_identity()
-        search_current_admin = mongo.db.admins.find_one({'email': current_user['email']})
+        search_current_admin = mongo.db.admins.find_one(
+            {'email': current_user['email']})
         if (search_current_admin):
             formData = dict(request.files)
             image = formData.get("image")
@@ -235,3 +250,26 @@ def upload_image():
             output['status'] = False
             output['message'] = "FORBIDDEN"
             return jsonify(output), 403
+
+
+@products.route('/products/search', methods=['GET'])
+def list_search():
+    if request.method == 'GET':
+        output = defaultObject()
+        query = request.args.get('search')
+        total_of_products = 0
+        for single_product in mongo.db.products.find({'name': {'$regex': query, '$options': 'i'}}):
+            if (single_product['available']):
+                total_of_products += 1
+                del single_product['description']
+                del single_product['updatedAt']
+                del single_product['createdAt']
+                del single_product['sold']
+                single_product['_id'] = str(single_product['_id'])
+                output['data'].append(single_product)
+        if total_of_products > 0:
+            output['status'] = True
+            return jsonify(output), 200
+        else:
+            output['message'] = 'ProductsNotFound'
+            return jsonify(output), 404
