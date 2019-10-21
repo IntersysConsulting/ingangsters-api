bf97973d8ffae14be0eafe33c12a38b5ae259564
Sofia Robles <srobles@intersysconsulting.com>
2019-10-21 12:45:00 -0500

    Endpoint works with success transaction

--- jsonschemas/checkout.py
+++ jsonschemas/checkout.py
@@ -4,24 +4,26 @@ from jsonschema.exceptions import ValidationError, SchemaError
 stripe_charge = {
     "type": "object",
     "properties": {
-        "amount": {
-            "type": "number"
-        },
         "customer": {
             "type": "object",
-            "email": {
-                "type": "string"
-            },
-            "source": {
-                "type": "string"
-            }
+            "email": {"type": "string"},
+            "source": {"type": "string"},
         },
-        "description": {
-            "type": "string"
-        }
+        "items": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "_id": {"type": "string"},
+                    "price": {"type": "number"},
+                    "quantity": {"type": "number"},
+                },
+                "required": ["_id", "price", "quantity"],
+            },
+        },
+        "description": {"type": "string"},
     },
-
-    "required": ["amount", "customer"]
+    "required": ["items", "customer"],
 }
 
 
@@ -29,7 +31,7 @@ def validate_stripe_charge(data):
     try:
         validate(data, stripe_charge)
     except ValidationError as e:
-        return {'ok': False, 'message': e}
+        return {"ok": False, "message": e}
     except SchemaError as e:
-        return {'ok': False, 'message': e}
-    return {'ok': True, 'data': data}
+        return {"ok": False, "message": e}
+    return {"ok": True, "data": data}
